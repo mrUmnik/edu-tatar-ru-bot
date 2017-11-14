2bfb36a90020d9e8a307124d94e5854fd9e08e95
mr.Umnik <mr.umnik@gmail.com>
2017-11-14 19:38:03 +0300

    Select one item and items collection from DB

--- src/Database.php
+++ src/Database.php
@@ -7,7 +7,7 @@ class Database
 
 	public function __construct($dbHost, $dbName, $dbLogin, $dbPassword)
 	{
-		$this->connection = new \PDO("mysql:host=$dbHost;dbname=$dbName", $dbLogin, $dbPassword);
+		$this->connection = new \PDO("mysql:host=$dbHost;dbname=$dbName", $dbLogin, $dbPassword, [\PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4']);
 		$this->connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 	}
 
@@ -15,4 +15,21 @@ class Database
 	{
 		return $this->connection;
 	}
+
+	public function selectMany($expression, $values = array())
+	{
+		$statement = $this->connection->prepare($expression);
+		$statement->setFetchMode(\PDO::FETCH_ASSOC);
+		$statement->execute($values);
+		$data = $statement->fetchAll();
+		return $data;
+	}
+    public function selectOne($expression, $values = array())
+    {
+        $statement = $this->connection->prepare($expression);
+        $statement->setFetchMode(\PDO::FETCH_ASSOC);
+        $statement->execute($values);
+        $data = $statement->fetch();
+        return $data;
+    }
 }
